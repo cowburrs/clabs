672ef7cc42487770a5e6d1de788cc7e4d7b2a2b6
chara3kki <charasu.mail@gmail.com>
2026-08-24 12:36:54 +1000

    Feat: Fixed camera movement and tested left and right mouse buttons

--- camerafollow.gd
+++ camerafollow.gd
@@ -1,12 +1,21 @@
 extends Camera2D
 
+# I don't know if this is how it's supposed to work but here are the variables I guess 
+
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _input(event: InputEvent) -> void:
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position = get_global_mouse_position()
-	print(get_global_mouse_position())
+	position = get_local_mouse_position()
+	
+	
+	
+	# print(get_global_mouse_position())
 	pass
